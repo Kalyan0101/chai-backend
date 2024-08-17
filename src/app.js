0c3212e75c523526314593524be4e09ba8da624a
Kalyan0101 <naskarkalyan2000@outlook.com>
2024-08-18 02:47:16 +0530

    working on utild

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+//  allow cros policy
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+// set json lenght limit
+app.use(express.json({
+    limit: "16kb"
+}))
+// config url properties
+app.use(express.urlencoded({
+    extended: true,
+    limit: "16kb"
+}))
+// use for, storing some specific files (image, *file, song) in localy
+app.use(express.static("public"))
+
+//  use for, perform CRED operation on cookies
+app.use(cookieParser())
+
+export { app }
---
+++ src/index.js
@@ -0,0 +1,50 @@
+// require ('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+.then( ()  => {
+    app.on("error", (err) => {
+        console.log("ERROR: DB not conected",err);
+        throw(err);
+        
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is Running at port: ${process.env.PORT}`);
+        
+    });
+} )
+.catch( (err) => {
+    console.log("MongoDB Connection faailed:", err);
+    
+} )
+
+/*
+import express from "express";
+const app = express()
+// ifies
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR: database not able to connect", error);
+            throw error;
+        })
+
+        app.listern(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);            
+        })
+
+    } catch (error) {        
+        console.log("ERROR", error);
+        throw err
+        
+    }
+})
+*/
