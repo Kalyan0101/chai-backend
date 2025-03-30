ab1206d5d5ccbd2cf048be57ed0d538245ced978
Kalyan0101 <naskarkalyan2000@outlook.com>
2025-03-30 23:29:31 +0530

    initialized project

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express, { json } from "express";
+import cors from "cors"
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// accept json data
+app.use(express.json({ 
+    limit: "16kb" 
+}))
+
+// accept data over url
+app.use(express.urlencoded({ 
+    extended: true, 
+    limit: "16kb"
+}))
+
+// store data locally
+app.use(express.static("public"))
+
+// for set cookies on client browser
+app.use(cookieParser())
+
+
+export { app }
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFileName) => {
+    try {
+        if (!localFileName) return null;
+
+        // upload file on cloudinary
+        const response = await cloudinary.uploader.upload(localFileName, {
+            resource_type: "auto",
+        });
+
+        // file has been uploaded successfully
+        console.log("file is uploaded on cloudinary", response);
+        return response;
+    }
+    catch (error) {
+        return null;
+    }
+    finally{
+        fs.unlinkSync(localFileName);    // remove locally saved file
+    }
+};
+
+export { uploadOnCloudinary }
